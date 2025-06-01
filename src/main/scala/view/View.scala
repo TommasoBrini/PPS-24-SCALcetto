0399e134d349e8a3a6b8a5ed84c35e1b4483ebd6
Tommaso <tommaso.brini@studio.unibo.it>
2025-06-01 22:05:57 +0200

    feat: implement simple gui

--- src/main/scala/view/View.scala
+++ src/main/scala/view/View.scala
@@ -1,7 +1,32 @@
 package view
 
+import scala.swing.*
+import java.awt.{Color, Graphics2D}
+
 import model.SimulationState
 
 object View:
-  def render(model: SimulationState): Unit =
-    println(s" ball pos: ${model.ball.position}")
+
+  class MatchPanel(var state: SimulationState) extends Panel:
+    override def paintComponent(g: Graphics2D): Unit =
+      super.paintComponent(g)
+      g.setColor(Color.RED)
+      g.fillOval(state.ball.position.x * 10, state.ball.position.y * 10, 10, 10)
+      g.setColor(Color.blue)
+      state.playerList.foreach(p =>
+        g.fillRect(p.position.x * 10, p.position.y * 10, 10, 10)
+      )
+
+  class SwingView(initialState: SimulationState):
+    private val panel = new MatchPanel(initialState)
+    panel.background = Color.green
+    private val frame = new MainFrame:
+      title = "SCALcetto"
+      contents = panel
+      size = new Dimension(700, 700)
+      centerOnScreen()
+      visible = true
+
+    def render(state: SimulationState): Unit =
+      panel.state = state
+      panel.repaint()
