f588b89a24cf56e1296b674e2fd0ee60819f67f4
Tommaso <tommaso.brini@studio.unibo.it>
2025-06-01 22:06:48 +0200

    chore: modify update version for testing

--- src/main/scala/SCALcetto.scala
+++ src/main/scala/SCALcetto.scala
@@ -2,6 +2,6 @@ import SimulationLoop.loop
 import model.*
 
 @main def SCALcetto =
-  val playerList: List[Player]         = List(Player(0, Position(0, 0), false))
-  val simulationState: SimulationState = SimulationState(playerList, Ball(Position(0, 0)))
-  loop(simulationState, 5)
+  val playerList: List[Player]         = List(Player(0, Position(10, 10), false), Player(1, Position(5, 30), false))
+  val simulationState: SimulationState = SimulationState(playerList, Ball(Position(50, 50)))
+  loop(simulationState, 1000)
