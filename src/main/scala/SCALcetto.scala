6b1b702d18f6b9e8e7b40334324731db1342e1ee
Tommaso <tommaso.brini@studio.unibo.it>
2025-06-01 18:22:42 +0200

    chore: create new branch for gui

--- src/main/scala/SCALcetto.scala
+++ src/main/scala/SCALcetto.scala
@@ -1,5 +1,5 @@
 import SimulationLoop.loop
-import model.{Ball, Event, Player, Position, SimulationState}
+import model.*
 
 @main def SCALcetto =
   val playerList: List[Player]         = List(Player(0, Position(0, 0), false))
