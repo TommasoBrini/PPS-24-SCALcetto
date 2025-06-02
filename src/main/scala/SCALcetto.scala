4604cd23e3d4212bdf1122e5144ad5095e9deae1
Tommaso <tommaso.brini@studio.unibo.it>
2025-06-02 18:11:17 +0200

    feat(gui): implement new simple version of view

--- src/main/scala/SCALcetto.scala
+++ src/main/scala/SCALcetto.scala
@@ -3,5 +3,5 @@ import model.*
 
 @main def SCALcetto =
   val playerList: List[Player]         = List(Player(0, Position(10, 10), false), Player(1, Position(5, 30), false))
-  val simulationState: SimulationState = SimulationState(playerList, Ball(Position(50, 50)))
+  val simulationState: SimulationState = SimulationState(playerList, Ball(Position(30, 30)))
   loop(simulationState, 1000)
--- src/main/scala/view/View.scala
+++ src/main/scala/view/View.scala
@@ -8,22 +8,56 @@ import model.SimulationState
 object View:
 
   class MatchPanel(var state: SimulationState) extends Panel:
+    preferredSize = new Dimension(600, 400)
     override def paintComponent(g: Graphics2D): Unit =
       super.paintComponent(g)
-      g.setColor(Color.RED)
+
+      val width  = size.width
+      val height = size.height
+
+      g.setColor(Color.GREEN)
+      g.fillRect(0, 0, width, height)
+
+      g.setColor(Color.WHITE)
+      val lineThickness = 5
+      g.fillRect(0, 0, width, lineThickness)
+      g.fillRect(0, height - lineThickness, width, lineThickness)
+      g.fillRect(0, 0, lineThickness, height)
+      g.fillRect(width - lineThickness, 0, lineThickness, height)
+      g.drawLine(width / 2, 0, width / 2, height)
+      g.drawOval(width / 2 - 50, height / 2 - 50, 100, 100)
+
+      g.setColor(Color.black)
+      val goalHeight = 50
+      val goalWidth  = 10
+      g.fillRect(0, (height - goalHeight) / 2, goalWidth, goalHeight)
+      g.fillRect(width - goalWidth, (height - goalHeight) / 2, goalWidth, goalHeight)
+
+      g.setColor(Color.white)
+      val areaWidth  = 60
+      val areaHeight = 200
+      g.drawRect(0, (height - areaHeight) / 2, areaWidth, areaHeight)                 // Area rigore sinistra
+      g.drawRect(width - areaWidth, (height - areaHeight) / 2, areaWidth, areaHeight) // Area rigore destra
+
+      g.setColor(Color.WHITE)
       g.fillOval(state.ball.position.x * 10, state.ball.position.y * 10, 10, 10)
+
       g.setColor(Color.blue)
       state.playerList.foreach(p =>
         g.fillRect(p.position.x * 10, p.position.y * 10, 10, 10)
       )
 
   class SwingView(initialState: SimulationState):
-    private val panel = new MatchPanel(initialState)
-    panel.background = Color.green
+    private val panel     = new MatchPanel(initialState)
+    private val infoLabel = new Label("SCALcetto - A simple soccer simulation")
     private val frame = new MainFrame:
       title = "SCALcetto"
-      contents = panel
-      size = new Dimension(700, 700)
+      contents = new BorderPanel {
+        layout(infoLabel) = BorderPanel.Position.North
+        layout(panel) = BorderPanel.Position.Center
+      }
+      size = new Dimension(600, 450)
+      resizable = false
       centerOnScreen()
       visible = true
 
