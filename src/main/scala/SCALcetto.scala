f4a202062ffc9b776d271a8858615772fab2c979
Tommaso <tommaso.brini@studio.unibo.it>
2025-06-09 16:14:38 +0200

    feat: change gui with fps mechanism

--- src/main/scala/SCALcetto.scala
+++ src/main/scala/SCALcetto.scala
@@ -5,4 +5,4 @@ import update.factory.SimulationFactory
 @main def SCALcetto =
 
   val initialState: SimulationState = SimulationFactory.initialSimulationState()
-  loop(initialState, 1000)
+  loop(initialState, 1000, 50)
--- src/main/scala/SimulationLoop.scala
+++ src/main/scala/SimulationLoop.scala
@@ -1,4 +1,4 @@
-import model.Model.{Event, SimulationState}
+import model.Model.*
 import update.Update.*
 import view.View.SwingView
 
@@ -6,21 +6,22 @@ import javax.swing.Timer as SwingTimer
 import java.awt.event.{ActionEvent, ActionListener}
 
 object SimulationLoop:
-  def loop(model: SimulationState, nStep: Int): Unit =
+  def loop(model: SimulationState, nStep: Int, frameRate: Int = 30): Unit =
+    val delayMs                  = 1000 / frameRate
     val view: SwingView          = new SwingView(model)
     var current: SimulationState = model
     var remainingSteps: Int      = nStep
 
     val timer: SwingTimer = new SwingTimer(
-      500,
-      new ActionListener {
-        override def actionPerformed(e: ActionEvent): Unit = {
+      delayMs,
+      new ActionListener:
+        override def actionPerformed(e: ActionEvent): Unit =
           if remainingSteps > 0 then
             current = update(current, Event.Step)
             view.render(current)
             remainingSteps -= 1
-        }
-      }
+          else
+            e.getSource.asInstanceOf[SwingTimer].stop()
     )
 
     timer.start()
