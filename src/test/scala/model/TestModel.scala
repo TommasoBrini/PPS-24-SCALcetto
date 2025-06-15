aeb2ef05338161ca1d5294a8c623abc97d29b5bb
Tommaso <tommaso.brini@studio.unibo.it>
2025-06-15 12:24:45 +0200

    test: complete coverage for model

--- src/test/scala/model/TestModel.scala
+++ src/test/scala/model/TestModel.scala
@@ -1,69 +1,54 @@
-//package model
-//
-//import config.FieldConfig
-//import model.Model.*
-//import org.scalatest.flatspec.AnyFlatSpec
-//import org.scalatest.matchers.should.Matchers
-//
-//class TestModel extends AnyFlatSpec with Matchers:
-//
-//  val player: Player = Player(1, Position(0, 0), PlayerStatus.noControl, None, Movement(Direction.none, 0))
-//
-//  "Position" should "store x and y coordinates" in:
-//    val position = Position(10, 20)
-//    position.x shouldBe 10
-//    position.y shouldBe 20
-//
-//  "Direction" should "expose x and y components" in:
-//    val direction = Direction(1, 0)
-//    direction.x shouldBe 1
-//    direction.y shouldBe 0
-//  it should "have a zero direction" in:
-//    val zeroDirection = Direction.none
-//    zeroDirection.x shouldBe 0
-//    zeroDirection.y shouldBe 0
-//
-//  "Movement" should "store direction and speed" in:
-//    val movement = Movement(Direction(1, 0), 5)
-//    movement.direction.x shouldBe 1
-//    movement.direction.y shouldBe 0
-//    movement.speed shouldBe 5
-//
-//  "A player" should "be created correctly" in:
-//    player.id shouldEqual 1
-//    player.position shouldEqual Position(0, 0)
-//    player.nextAction shouldEqual None
-//
-//  it should "have a movement with zero direction and speed" in:
-//    player.movement.direction shouldEqual Direction.none
-//    player.movement.speed shouldEqual 0
-//
-//  it should "allow setting a next action" in:
-//    val action: Action = Action.Move(Direction(1, 1), FieldConfig.playerSpeed)
-//    val updatedPlayer  = player.copy(nextAction = Some(action))
-//    player.nextAction shouldEqual None
-//    updatedPlayer.nextAction shouldEqual Some(action)
-//
-//  it should "take control of the ball" in:
-//    val updatedPlayer = player.copy(status = PlayerStatus.ballControl)
-//    updatedPlayer.status shouldEqual PlayerStatus.ballControl
-//
-//  "A team" should "be created with players" in:
-//    val second_player: Player = Player(2, Position(1, 1), PlayerStatus.noControl, None, Movement(Direction.none, 0))
-//    val team: Team            = Team(1, List(player, second_player))
-//    team.players shouldEqual List(player, second_player)
-//
-//  "A ball" should "be created with a position and movement" in:
-//    val ball: Ball = Ball(Position(0, 0), Movement(Direction.none, 0))
-//    ball.position shouldEqual Position(0, 0)
-//    ball.movement.direction shouldEqual Direction.none
-//    ball.movement.speed shouldEqual 0
-//
-//  "A simulation state" should "contain teams and a ball" in:
-//    val second_player: Player = Player(2, Position(1, 1), PlayerStatus.noControl, None, Movement(Direction.none, 0))
-//    val team1: Team           = Team(1, List(player))
-//    val team2: Team           = Team(2, List(second_player))
-//    val ball: Ball            = Ball(Position(0, 0), Movement(Direction.none, 0))
-//    val state: MatchState     = MatchState(List(team1, team2), ball)
-//    state.teams shouldEqual List(team1, team2)
-//    state.ball shouldEqual ball
+package model
+
+import Model.*
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class TestModel extends AnyFlatSpec with Matchers:
+
+  private val initial_x_position: Int = 3
+  private val initial_y_position: Int = 4
+
+  "Direction" should "be create from Position" in:
+    val pos: Position  = Position(initial_x_position, initial_y_position)
+    val dir: Direction = Direction(pos)
+    dir.x shouldEqual initial_x_position.toDouble
+    dir.y shouldEqual initial_y_position.toDouble
+  it should "be create from coordinates" in:
+    val dir: Direction = Direction(initial_x_position, initial_y_position)
+    dir.x shouldEqual initial_x_position.toDouble
+    dir.y shouldEqual initial_y_position.toDouble
+  it should "be none" in:
+    val dir: Direction = Direction.none
+    dir.x shouldEqual 0.0
+    dir.y shouldEqual 0.0
+
+  "Movement" should "be still" in:
+    val movement: Movement = Movement.still
+    movement.direction shouldEqual Direction.none
+    movement.speed shouldEqual 0
+
+  "Position" should "get direction to another position" in:
+    val pos1: Position = Position(1, 1)
+    val pos2: Position = Position(4, 5)
+    val dir: Direction = pos1.getDirection(pos2)
+    dir.x shouldEqual 0.6
+    dir.y shouldEqual 0.8
+  it should "apply movement" in:
+    val pos: Position      = Position(initial_x_position, initial_y_position)
+    val movement: Movement = Movement(Direction(1, 1), 3)
+    val newPos: Position   = pos + movement
+    newPos.x shouldEqual (initial_x_position + 3)
+    newPos.y shouldEqual (initial_y_position + 3)
+
+  "Player" should "be create without ball" in:
+    val player: Player = Player(1, Position(0, 0), Movement.still)
+    player.id shouldEqual 1
+    player.position shouldEqual Position(0, 0)
+    player.movement shouldEqual Movement.still
+    player.ball shouldEqual None
+    player.nextAction shouldEqual None
+  it should "be create with ball" in:
+    val ball: Ball     = Ball(Position(1, 1), Movement.still)
+    val player: Player = Player(1, Position(0, 0), Movement.still, Some(ball), Some(Action.Take(ball)))
+    player.hasBall shouldEqual true
