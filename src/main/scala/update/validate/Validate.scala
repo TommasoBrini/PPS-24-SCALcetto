5eb68814150228e735f6ab9b069f4c68f7f1a771
Tommaso <tommaso.brini@studio.unibo.it>
2025-06-20 11:57:57 +0200

    refactor: move toAction in validate phase

--- src/main/scala/update/validate/Validate.scala
+++ src/main/scala/update/validate/Validate.scala
@@ -1,7 +1,10 @@
 package update.validate
 
-import model.Match.Action.Hit
-import model.Match.{MatchState, Player, Team}
+import model.Match.{Action, Decision, MatchState, Player, Team}
+import Decision.*
+import config.FieldConfig
+
+import scala.util.Random
 
 object Validate {
   def validate(state: MatchState): MatchState =
@@ -10,7 +13,25 @@ object Validate {
   def validate(team: Team): Team =
     Team(team.id, team.players.map(validate))
 
-  def validate(player: Player): Player = player.decision match
-    case _ => player.copy(nextAction = player.decision.toAction)
+  def validate(player: Player): Player =
+    player.copy(nextAction =
+      if Random.nextDouble() < getSuccessRate(player.decision) then getSuccessAction(player.decision)
+      else getFailureAction(player.decision)
+    )
+
+  private def getSuccessRate(decision: Decision): Double =
+    decision match
+      case Pass(from, to) => 1
+      case _              => 1
+
+  private def getSuccessAction(decision: Decision): Action =
+    decision match
+      case Pass(from, to) => Action.Hit(from.position.getDirection(to.position), FieldConfig.ballSpeed)
+      case _              => Action.Initial
+
+  private def getFailureAction(decision: Decision): Action =
+    decision match
+      case Pass(from, to) => Action.Hit(from.position.getDirection(to.position), FieldConfig.ballSpeed)
+      case _              => Action.Initial
 
 }
