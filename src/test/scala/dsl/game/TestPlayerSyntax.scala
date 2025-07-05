f2dacc66df0f66f250b05b2cf846f30c2b18036e
Tommaso <tommaso.brini@studio.unibo.it>
2025-07-05 20:19:30 +0200

    chore: fix bugs after merge

--- src/test/scala/dsl/game/TestPlayerSyntax.scala
+++ src/test/scala/dsl/game/TestPlayerSyntax.scala
@@ -6,16 +6,16 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.matchers.should.Matchers.shouldBe
 import model.Match.{Ball, Player, Team}
 import model.Space.Bounce.*
-import model.decisions.PlayerDecisionFactory.*
-import PlayerSyntax.*
+import model.decisions.PlayerRoleFactory.*
+import dsl.game.PlayerSyntax.*
 
 import scala.util.Random
 
 class TestPlayerSyntax extends AnyFlatSpec with Matchers:
   "A Player" should "correctly report hasBall when carrying a ball" in:
     val ball: Ball        = Ball(Position(5, 5), Movement(Direction.none, 0))
-    val playerWithBall    = Player(1, Position(0, 0), Movement.still, Some(ball)).asControlDecisionPlayer
-    val playerWithoutBall = Player(2, Position(0, 0), Movement.still)
+    val playerWithBall    = Player(1, Position(0, 0), Movement.still, Some(ball)).asAttackingPlayer
+    val playerWithoutBall = Player(2, Position(0, 0), Movement.still).asDefendingPlayer
 
     playerWithBall.ball.isDefined shouldBe playerWithBall.hasBall
     playerWithoutBall.ball.isDefined shouldBe playerWithoutBall.hasBall
