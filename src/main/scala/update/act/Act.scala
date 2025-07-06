f99832709cb05928ffb7f3fd0930f54bc4462b84
Tommaso <tommaso.brini@studio.unibo.it>
2025-07-06 13:01:39 +0200

    feat: resolve movement out of field

--- src/main/scala/update/act/Act.scala
+++ src/main/scala/update/act/Act.scala
@@ -85,7 +85,9 @@ object Act:
       case Stopped(duration)      => player.copy(movement = Movement.still)
       case Take(ball)             => player.copy(movement = Movement.still)
       case _                      => player
-    def move(): Player = player.copy(position = player.position + player.movement)
+    def move(): Player =
+      val newPosition = (player.position + player.movement).clampToField
+      player.copy(position = newPosition)
 
   extension (ball: Ball)
     def updateMovement(carrier: Option[Player]): Ball = carrier match
