5a486f7e660310d5e994ebbb6761298e41d1a974
Mounir Samite <mounir.samite@gmail.com>
2025-07-07 12:39:11 +0200

    chore: add BallBuilder Tests

--- src/test/scala/dsl/creation/build/TestBallBuilder.scala
+++ src/test/scala/dsl/creation/build/TestBallBuilder.scala
@@ -1,3 +1,26 @@
 package dsl.creation.build
 
-class TestBallBuilder {}
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers.*
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.matchers.should.Matchers.shouldBe
+import model.Space.{Direction, Movement, Position}
+import org.scalatest.matchers.should.Matchers
+
+class TestBallBuilder extends AnyFlatSpec with Matchers {
+
+  "BallBuild " should " build a Ball with the chosen position and movement" in:
+    val builder = BallBuilder()
+      .at(10, 20)
+      .move(Direction(1, -1), 3)
+
+    val ball = builder.build()
+
+    ball.position shouldBe Position(10, 20)
+    ball.movement shouldBe Movement(Direction(1, -1), 3)
+
+  "BallBuild defaults " should " be origin & still movement when nothing is set" in:
+    BallBuilder().build() shouldBe model.Match.Ball(Position(0, 0), Movement.still)
+
+}
